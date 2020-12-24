32c9bf6c0d75542413eec41afdad4ab093b65c1a
Cuong Tran <103cuong@gmail.com>
2020-12-24 16:35:19 +0700

    feat: kafkajs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,42 @@
-import { name } from 'faker';
-import delay from 'delay';
+const { Kafka } = require('kafkajs');
+const delay = require('delay');
 
-import { subscribe } from './pkg/consumer';
-import { publish } from './pkg/producer';
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['0.0.0.0:9092'],
+});
 
-(async () => {
-  const TOPIC = 'MEOW_MOEW';
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'test-group' });
 
-  publish(TOPIC, name.jobTitle());
-  await delay(1234);
-  subscribe(TOPIC);
-})();
+const run = async () => {
+  // Producing
+  await producer.connect();
+
+  // setInterval(() => {
+  //   producer.send({
+  //     topic: 'test-topic',
+  //     messages: [
+  //       { value: '💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩' },
+  //     ],
+  //   })
+  // }, 5000);
+
+  // Consuming
+  await consumer.connect();
+  // await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+  await consumer.subscribe({ topic: 'test-topic' });
+
+  await consumer.run({
+    // @ts-ignore
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      });
+    },
+  });
+};
+
+run().catch(console.error);
